9452129d2d4f1f24e366406983279a0c36aa899e
AJ M <aj.malcolm@hotmail.ca>
2020-01-12 00:34:19 -0500

    Updated main title location and font size, and menu bar for mobile.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
-function myFunction() {
+function toggleNavMenu(bool) {
   var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  if (bool) {
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
   } else {
-    x.className = "topnav";
+    x.className = "topnav"
   }
 }
 
 function openTab(tabName) {
   $('.active').removeClass('active');
   $('#'+tabName).toggleClass('active')
+  toggleNavMenu(false)
 }
 
 // Get the element with id="defaultOpen" and click on it
