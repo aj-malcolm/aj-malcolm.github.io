e65a67925cc0a6e7bcb16c0c2a55956a4c14acc5
AJ M <aj.malcolm@hotmail.ca>
2020-01-14 13:26:48 -0500

    Completed mobile site initial layout.

--- js/script.js
+++ js/script.js
@@ -1,21 +1,27 @@
-function toggleNavMenu(bool) {
-  var x = document.getElementById("myTopnav");
-  if (bool) {
-    if (x.className === "topnav") {
-      x.className += " responsive";
+function toggleActive(tabName) {
+  $('.active').removeClass('nav-options_active');
+  $('#'+tabName).toggleClass('nav-options_active')
+  document.getElementById("myNav").style.display = "none";
+}
+
+function openNav() {
+  document.getElementById("myNav").style.display = "block";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.display = "none";
+} 
+
+/* Trigger home icon display when no longer on home screen */
+$(window).on('scroll', function() {
+    var element_position = ($('#main-body').offset().top) / 1.5;
+    var y_scroll_pos = window.pageYOffset;
+    var scroll_pos_test = element_position;
+
+    if(y_scroll_pos > scroll_pos_test) {
+      $('.nav-home').css('display', 'block')
     } else {
-      x.className = "topnav";
+      $('.nav-home').css('display', 'none')
     }
-  } else {
-    x.className = "topnav"
-  }
-}
-
-function openTab(tabName) {
-  $('.active').removeClass('active');
-  $('#'+tabName).toggleClass('active')
-  toggleNavMenu(false)
-}
-
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("home").click();
+});
