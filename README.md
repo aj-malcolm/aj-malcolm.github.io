4f247b913cc3fe40aa57d9aad55c0cdef17ff0d2
AJ M <aj.malcolm@hotmail.ca>
2020-01-22 14:08:55 -0500

    updated with mobile-first design and new nav bars.

---
+++ README.md
@@ -0,0 +1,11 @@
+<https://developer.mozilla.org/en-US/docs/Learn/Common_questions/set_up_a_local_testing_server>
+
+```python
+# If Python version returned above is 3.X
+python3 -m http.server
+# On windows try "python" instead of "python3"
+# If Python version returned above is 2.X
+python -m SimpleHTTPServer
+```
+
+By default, this will run the contents of the directory on a local web server, on port 8000. You can go to this server by going to the URL ```localhost:8000``` in your web browser. Here you'll see the contents of the directory listed — click the HTML file you want to run.
--- js/script.js
+++ js/script.js
@@ -1,34 +1,28 @@
-function toggleActive(tabName) {
-  $('.active').removeClass('nav-options_active');
-  $('#'+tabName).toggleClass('nav-options_active')
-  closeNavOnSelect();
+function changeText(){
+  if(document.getElementById('nav_btn').checked) {
+    $('#nav_label').text('CLOSE')
+  } else {
+    $('#nav_label').text('MENU')
+  }
 }
 
 function toggleNav() {
-  var x =document.getElementById('myNav')
-  var window_size = window.matchMedia("(max-width: 850px)")
-  if (window_size.matches) {
-    if (x.className === 'nav-options') {
-      x.className += ' display';
-    } else {
-      x.className = "nav-options";
-    }
-  }
+  $('#nav_btn').click()
 }
 
-/* Trigger home icon display when no longer on home screen */
-$(window).on('scroll', function() {
-  var mobile = window.matchMedia("(max-width: 850px)")
-  if (mobile.matches) {
-    var element_position = ($('#main-body').offset().top) / 1.5;
-    var y_scroll_pos = window.pageYOffset;
-    var scroll_pos_test = element_position;
-    var x =document.getElementById('nav-home_button')
+// /* Trigger home icon display when no longer on home screen */
+// $(window).on('scroll', function() {
+//   var mobile = window.matchMedia("(max-width: 850px)")
+//   if (mobile.matches) {
+//     var element_position = ($('#main-body').offset().top) / 1.5;
+//     var y_scroll_pos = window.pageYOffset;
+//     var scroll_pos_test = element_position;
+//     var x =document.getElementById('nav-home_button')
 
-    if(y_scroll_pos > scroll_pos_test) {
-      x.className += ' display';
-    } else {
-      x.className = "nav-home";
-    }
-  }
-});
+//     if(y_scroll_pos > scroll_pos_test) {
+//       x.className += ' display';
+//     } else {
+//       x.className = "nav-home";
+//     }
+//   }
+// });
